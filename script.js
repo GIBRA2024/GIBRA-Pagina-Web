9c158bc9cfc977d76432a8ee2ab84449bedc1cbf
Mateo <monorymateo1@gmail.com>
2025-03-09 17:16:44 -0500

    intento coneccion

--- script.js
+++ script.js
@@ -23,3 +23,10 @@ cerrar.addEventListener("click", () => {
     bloqueo.classList.remove('bloqueo-activo');
 })
 
+function loadScript(a) {
+    var b = document.getElementsByTagName("head")[0], c = document.createElement("script");
+    c.type = "text/javascript", c.src = "https://tracker.metricool.com/resources/be.js", c.onreadystatechange = a,c.onload = a,b.appendChild(c)
+}
+loadScript(function () {
+    beTracker.t({hash: "b6b36abfeca41b174962054b1e4cb8ce"})
+});
